89687f559ff91f00e07af97aaf47f9a426d95682
Alexandre Jung <jungalexandre@ymail.com>
2022-02-13 02:25:13 +0100

    :sparkles: add search algorithm

--- src/js/lib/search.js
+++ src/js/lib/search.js
@@ -1,4 +1,21 @@
 /* eslint-disable no-unused-vars */
+import { removeDiacritics, slugify } from 'utils/string';
+
+function normalizedContains(needle, haystack) {
+  const regexp = new RegExp(removeDiacritics(needle), 'gi');
+  return regexp.test(removeDiacritics(haystack));
+}
+
+function normalizedEquals(string1, string2) {
+  return slugify(string1) == slugify(string2);
+}
+
+function normalizedArrayIncludes(needle, haystack) {
+  for (const haystackItem of haystack) {
+    if (normalizedEquals(needle, haystackItem)) return true;
+  }
+  return false;
+}
 
 export default {
   /**
@@ -24,6 +41,53 @@ export default {
      * },
      */
     // TODO write search algorithm here.
+
+    /**
+     * Algorithm:
+     *
+     * Based on filter(), map() and every() to check if a recipe fulfills requirements.
+     * Comparisons are case-insensitive and ignore accents.
+     *
+     * 1. Filter all recipes.
+     *    A recipe matches if:
+     *      - All search words appear either in recipe name, ingredients or description.
+     *        This is an inclusion test, so 'chocolat' matches with 'chocolat', 'chocolat au lait', 'chocolat noir', ...
+     *        -> Main search is more permissive than keyword filtering.
+     *      - All keywords appear in their corresponding category (ingredients, appliance, utensils).
+     *        This is an equality test, so 'chocolat' matches only the exact string 'chocolat'.
+     *        -> Keyword filtering is more restrictive than main search.
+     *    If search is empty, all recipes matches. See the note below.
+     * 2. Map the result to return an array of IDs.
+     *
+     * ❗ Using every() in checks works because it returns true on empty arrays.
+     * This allows to keep all recipes when there is no search or keywords.
+     */
+
+    return recipes
+      .filter(({ name, description, ingredients, appliance, utensils }) => {
+        return (
+          search
+            .map((searchWord) => {
+              return (
+                normalizedContains(searchWord, name) ||
+                normalizedContains(searchWord, ingredients.map(({ ingredient }) => ingredient).join(' ')) ||
+                normalizedContains(searchWord, description)
+              );
+            })
+            .every((v) => v) &&
+          keywords.ingredients
+            .map((ingredient) =>
+              normalizedArrayIncludes(
+                ingredient,
+                ingredients.map(({ ingredient }) => ingredient)
+              )
+            )
+            .every((v) => v) &&
+          keywords.appliances.map((_appliance) => normalizedEquals(_appliance, appliance)).every((v) => v) &&
+          keywords.utensils.map((utensil) => normalizedArrayIncludes(utensil, utensils)).every((v) => v)
+        );
+      })
+      .map(({ id }) => id);
   },
 };
 
